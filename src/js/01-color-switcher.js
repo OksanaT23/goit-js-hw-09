58734202c3c7b43f59895fe6a6c8c035b95d3e35
OksanaT23 <129532941+OksanaT23@users.noreply.github.com>
2023-09-18 18:38:57 +0300

    hw 9.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let switcherId = null;
+
+function changeColor() {
+    const color = getRandomHexColor();
+
+    document.body.style.backgroundColor = color;
+}
+
+startBtn.addEventListener('click', function () {
+    switcherId = setInterval(changeColor, 1000);
+});
+stopBtn.addEventListener('click', function () {
+    clearInterval(switcherId);
+});
