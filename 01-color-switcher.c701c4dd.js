213bac870adfde22d96155f4777326d65e4e9072
OksanaT23 <129532941+OksanaT23@users.noreply.github.com>
2023-09-20 10:37:41 +0000

    Deploying to gh-pages from @ OksanaT23/goit-js-hw-09@2335ea6351aedf26c2d4ac5c53de6cfc049e18f8 🚀

---
+++ 01-color-switcher.c701c4dd.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null;function a(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t}t.addEventListener("click",(function(){e&&clearInterval(e),e=setInterval(a,1e3)})),n.addEventListener("click",(function(){clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.c701c4dd.js.map
